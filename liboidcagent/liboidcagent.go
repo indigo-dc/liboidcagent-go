bf07bd8d05197e1c49f74a907e1ecdd52482b444
zachmann <gabriel.zachmann@kit.edu>
2019-03-18 11:26:18 +0100

    small refactoring

--- liboidcagent/liboidcagent.go
+++ liboidcagent/liboidcagent.go
@@ -26,36 +26,48 @@ func GetAccessToken(accountname string, min_valid_period uint64, scope string, a
 	return tokenResponse.Token, err
 }
 
-func GetTokenResponse(accountname string, min_valid_period uint64, scope string, application_hint string) (tokenResponse TokenResponse, e error) {
+func communicateWithSock(request string) (response []byte, e error) {
 	socketValue, socketSet := os.LookupEnv("OIDC_SOCK")
 	if !socketSet {
 		fmt.Fprintln(os.Stderr, "$OIDC_SOCK not set")
-		return tokenResponse, errors.New("$OIDC_SOCK not set")
+		return response, errors.New("$OIDC_SOCK not set")
 	}
 
 	c, err := net.Dial("unix", socketValue)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "could not connect to socket %s: %s\n", socketValue, err.Error())
-		return tokenResponse, err
+		return response, err
 	}
 	defer c.Close()
 
-	ipcReq := fmt.Sprintf(`{"request":"access_token","account":"%s","min_valid_period":%d,"scope":"%s","application_hint":"%s"}`, accountname, min_valid_period, scope, application_hint)
-	_, err = c.Write([]byte(ipcReq))
+	_, err = c.Write([]byte(request))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "could not write to socket %s: %s\n", socketValue, err.Error())
-		return tokenResponse, err
+		return response, err
 	}
-	var response = [4096]byte{}
-	length, err := c.Read(response[0:4095])
+	var res = [4096]byte{}
+	length, err := c.Read(res[0:4095])
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "could not read from socket %s: %s\n", socketValue, err.Error())
+		return response, err
+	}
+	res[length] = 0
+	return res[:length], nil
+}
+
+//TODO error handling
+func GetTokenResponse(accountname string, min_valid_period uint64, scope string, application_hint string) (tokenResponse TokenResponse, e error) {
+
+	//TODO scope and application hint only needed if not empty
+	ipcReq := fmt.Sprintf(`{"request":"access_token","account":"%s","min_valid_period":%d,"scope":"%s","application_hint":"%s"}`, accountname, min_valid_period, scope, application_hint)
+
+	response, err := communicateWithSock(ipcReq)
+	if err != nil {
 		return tokenResponse, err
 	}
 
-	response[length] = 0
 	var res tmp_TokenResponse
-	jsonErr := json.Unmarshal(response[0:length], &res)
+	jsonErr := json.Unmarshal(response, &res)
 	if jsonErr != nil {
 		fmt.Fprintf(os.Stderr, "error parsing the response from oidc-agent: %s\n", jsonErr)
 		return tokenResponse, jsonErr
