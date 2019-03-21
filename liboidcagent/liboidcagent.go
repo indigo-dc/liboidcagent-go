3957f3b127161a4daa9e4e389759153861f0b542
Lukas Burgey <ubedv@student.kit.edu>
2019-03-21 14:13:39 +0100

    Make naming go conforming

--- liboidcagent/liboidcagent.go
+++ liboidcagent/liboidcagent.go
@@ -10,38 +10,42 @@ import (
 	"time"
 )
 
+// TokenResponse is a parsed response from the oidc-agent
 type TokenResponse struct {
-	Token      string
-	Issuer     string
-	Expires_At time.Time
+	Token     string
+	Issuer    string
+	ExpiresAt time.Time
 }
 
-type tmp_TokenResponse struct {
-	Token      string `json:"access_token"`
-	Issuer     string `json:"issuer"`
-	Expires_At int64  `json:"expires_at"`
+type rawTokenResponse struct {
+	Token     string `json:"access_token"`
+	Issuer    string `json:"issuer"`
+	ExpiresAt int64  `json:"expires_at"`
+
+	Status string `json:"status,omitempty"`
+	Error  string `json:"error,omitempty"`
 }
 
-func _createTokenRequest(requestPartAccIss string, min_valid_period uint64, scope string, application_hint string) string {
+func _createTokenRequest(requestPartAccIss string, minValidPeriod uint64, scope string, applicationHint string) string {
 	requestPartScope := ""
 	if scope != "" {
 		requestPartScope = fmt.Sprintf(`,"scope":"%s"`, scope)
 	}
 	requestPartApplicationHint := ""
-	if application_hint != "" {
-		requestPartScope = fmt.Sprintf(`,"application_hint":"%s"`, application_hint)
+	if applicationHint != "" {
+		requestPartScope = fmt.Sprintf(`,"applicationHint":"%s"`, applicationHint)
 	}
-	return fmt.Sprintf(`{"request":"access_token"%s,"min_valid_period":%d%s%s}`, requestPartAccIss, min_valid_period, requestPartScope, requestPartApplicationHint)
+	return fmt.Sprintf(`{"request":"access_token"%s,"minValidPeriod":%d%s%s}`, requestPartAccIss, minValidPeriod, requestPartScope, requestPartApplicationHint)
 }
 
-func createTokenRequestAccount(accountname string, min_valid_period uint64, scope string, application_hint string) string {
+func createTokenRequestAccount(accountname string, minValidPeriod uint64, scope string, applicationHint string) string {
 	requestPartAcc := fmt.Sprintf(`,"account":"%s"`, accountname)
-	return _createTokenRequest(requestPartAcc, min_valid_period, scope, application_hint)
+	return _createTokenRequest(requestPartAcc, minValidPeriod, scope, applicationHint)
 }
 
-func createTokenRequestIssuer(issuer string, min_valid_period uint64, scope string, application_hint string) string {
+func createTokenRequestIssuer(issuer string, minValidPeriod uint64, scope string, applicationHint string) string {
 	requestPartIss := fmt.Sprintf(`,"issuer":"%s"`, issuer)
-	return _createTokenRequest(requestPartIss, min_valid_period, scope, application_hint)
+	return _createTokenRequest(requestPartIss, minValidPeriod, scope, applicationHint)
 }
 
 func communicateWithSock(request string) (response []byte, err error) {
@@ -70,43 +74,56 @@ func communicateWithSock(request string) (response []byte, err error) {
 	return
 }
 
-func parseIpcResponse(response []byte) (tokenResponse TokenResponse, e error) {
-	var res tmp_TokenResponse
-	jsonErr := json.Unmarshal(response, &res)
-	if jsonErr != nil {
-		fmt.Fprintf(os.Stderr, "error parsing the response from oidc-agent: %s\n", jsonErr)
-		return tokenResponse, jsonErr
+func parseIpcResponse(response []byte) (tokenResponse TokenResponse, err error) {
+	var res rawTokenResponse
+	err = json.Unmarshal(response, &res)
+	if err != nil {
+		err = fmt.Errorf("Unable to unmarshal: %s", response)
+		return
 	}
-	tokenResponse.Token = res.Token
-	tokenResponse.Issuer = res.Issuer
-	tokenResponse.Expires_At = time.Unix(res.Expires_At, 0)
-	return tokenResponse, e
+	if res.Error != "" {
+		err = fmt.Errorf("Agent error: %s", res.Error)
+		return
+	}
+	if res.Status == "failure" {
+		err = fmt.Errorf("status is \"failure\"")
+		return
+	}
+	tokenResponse = TokenResponse{
+		Token:     res.Token,
+		Issuer:    res.Issuer,
+		ExpiresAt: time.Unix(res.ExpiresAt, 0),
+	}
+	return
 }
 
-func GetTokenResponse(accountname string, min_valid_period uint64, scope string, application_hint string) (tokenResponse TokenResponse, e error) {
-	ipcReq := createTokenRequestAccount(accountname, min_valid_period, scope, application_hint)
+func GetTokenResponse(accountname string, minValidPeriod uint64, scope, applicationHint string) (resp TokenResponse, err error) {
+	ipcReq := createTokenRequestAccount(accountname, minValidPeriod, scope, applicationHint)
+	ipcResponse, err := communicateWithSock(ipcReq)
+	if err != nil {
+		return
+	}
+	resp, err = parseIpcResponse(ipcResponse)
+	return
+}
+
+func GetTokenResponseByIssuerUrl(issuer string, minValidPeriod uint64, scope, applicationHint string) (tokenResponse TokenResponse, err error) {
+	ipcReq := createTokenRequestIssuer(issuer, minValidPeriod, scope, applicationHint)
 	response, err := communicateWithSock(ipcReq)
 	if err != nil {
-		return tokenResponse, err
+		err = fmt.Errorf("Communicating with socket: %s", err)
+		return
 	}
-	return parseIpcResponse(response)
+	tokenResponse, err = parseIpcResponse(response)
+	return
 }
 
-func GetTokenResponseByIssuerUrl(issuer string, min_valid_period uint64, scope string, application_hint string) (tokenResponse TokenResponse, e error) {
-	ipcReq := createTokenRequestIssuer(issuer, min_valid_period, scope, application_hint)
-	response, err := communicateWithSock(ipcReq)
-	if err != nil {
-		return tokenResponse, err
-	}
-	return parseIpcResponse(response)
-}
-
-func GetAccessToken(accountname string, min_valid_period uint64, scope string, application_hint string) (token string, err error) {
-	tokenResponse, err := GetTokenResponse(accountname, min_valid_period, scope, application_hint)
+func GetAccessToken(accountname string, minValidPeriod uint64, scope, applicationHint string) (token string, err error) {
+	tokenResponse, err := GetTokenResponse(accountname, minValidPeriod, scope, applicationHint)
 	return tokenResponse.Token, err
 }
 
-func GetAccessTokenByIssuerUrl(issuer_url string, min_valid_period uint64, scope string, application_hint string) (token string, err error) {
-	tokenResponse, err := GetTokenResponseByIssuerUrl(issuer_url, min_valid_period, scope, application_hint)
+func GetAccessTokenByIssuerUrl(issuerURL string, minValidPeriod uint64, scope, applicationHint string) (token string, err error) {
+	tokenResponse, err := GetTokenResponseByIssuerUrl(issuerURL, minValidPeriod, scope, applicationHint)
 	return tokenResponse.Token, err
 }
